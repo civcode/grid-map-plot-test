3139846dd81d043bc8ec95119eeffb8d3fc54630
chris <ivi3001@yahoo.de>
2025-06-25 19:04:32 +0200

    added offscreen rendering and gui controls

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <random>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -40,11 +41,31 @@ int main() {
               << " with " << channels << " channels.\n";
 
 
-    for (int i = 0; i < width * height * channels; ++i) {
-        // std::cout << static_cast<int>(data.get()[i]) << " ";
-        // if ((i+1) % width == 0) {
-        //     std::cout << "\n";
-        // }
+    /** Create ROS-style Occupancy Grid Map 
+     * - int8[] data ... probability [0,100], unknown = -1
+    */
+    struct MapMetaData {
+        int width;
+        int height;
+        float resolution; // meters per pixel
+        float origin_x;  // origin in meters
+        float origin_y;  // origin in meters
+    };
+    MapMetaData map_metadata {
+        .width = width,
+        .height = height,
+        .resolution = 0.1f, // 0.1 m per cell
+        .origin_x = 0.0f,   // origin at (0,0)
+        .origin_y = 0.0f    // origin at (0,0)
+    };
+    std::unique_ptr<int8_t[]> occupancy_grid = std::make_unique<int8_t[]>(width * height);
+
+    for (int i = 0; i < width * height; ++i) {
+        if (data.get()[i] == 0) {
+            occupancy_grid[i] = 100;
+        } else {
+            occupancy_grid[i] = 0;
+        }
     }
     
     int px_per_cell = 10;
@@ -52,20 +73,16 @@ int main() {
     int grid_height = height * px_per_cell;
     std::cout << "Grid size: " << grid_width << "x" << grid_height << "\n";
 
-    RenderModule::Init(1200, 1200, 5.0);
-    RenderModule::EnableParentWindowDocking();
+    RenderModule::Init(1200, 1200, 0.0);
+    RenderModule::EnableRootWindowDocking();
 
-
-
-
-    NVGcontext *vg = RenderModule::GetNanoVGContext();
 
     /* Create a FB and draw to it */
     int fb_width = width*px_per_cell;
     int fb_height = height*px_per_cell;
     
     // NVGLUframebuffer* fb = nvgluCreateFramebuffer(vg, width, height, NVG_IMAGE_REPEATX | NVG_IMAGE_REPEATY | NVG_IMAGE_FLIPY);
-    nvg::SetContext(vg);
+    nvg::SetContext(RenderModule::GetNanoVGContext());
     NVGLUframebuffer* fb = nvg::GLUtilsCreateFramebuffer(fb_width, fb_height, 
         // NVG_IMAGE_REPEATX | NVG_IMAGE_REPEATY | 
         // NVG_IMAGE_FLIPY
@@ -82,81 +99,80 @@ int main() {
     glad::glViewport(0, 0, fb_width, fb_height);
     glad::glClearColor(0, 0, 0, 0);
     glad::glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    // glViewport(0, 0, width, height);
-    // glClearColor(0, 0, 0, 0);
-    // glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
-    nvgBeginFrame(vg, fb_width, fb_height, 1.0f);
+    /* Draw Grid Map */
+    nvg::BeginFrame(fb_width, fb_height, 1.0f);
 
+        nvg::BeginPath();
+        nvg::Rect(0, 0, static_cast<float>(grid_width), static_cast<float>(grid_height));
+        nvg::FillColor(nvg::RGBAf(1.0f, 1.0f, 1.0f, 1.0f));
+        nvg::Fill();
+
+        for (int i = 0; i < width * height; ++i) {
+            int x = (i % width) * px_per_cell;
+            int y = (i / width) * px_per_cell;
+            y = grid_height - y - px_per_cell; // Invert y-axis for correct rendering
+            int8_t value = occupancy_grid.get()[i];
+            if (value == 0) {
+                continue;
+            }
             nvg::BeginPath();
-            nvg::Rect(0, 0, static_cast<float>(grid_width), static_cast<float>(grid_height));
-            nvg::FillColor(nvg::RGBAf(1.0f, 1.0f, 1.0f, 1.0f));
+            nvg::Rect(static_cast<float>(x), static_cast<float>(y), static_cast<float>(px_per_cell), static_cast<float>(px_per_cell));
+            nvg::FillColor(nvg::RGBAf(0.6f, 0.6f, 0.6f, 1.0f));
             nvg::Fill();
-            nvg::ClosePath();
+        }
 
-            for (int i = 0; i < width * height; ++i) {
-                int x = (i % width) * px_per_cell;
-                int y = (i / width) * px_per_cell;
-                y = grid_height - y - px_per_cell; // Invert y-axis for correct rendering
-                unsigned char value = data.get()[i];
-                if (value != 0) {
-                    continue;
-                }
-                nvg::BeginPath();
-                nvg::Rect(static_cast<float>(x), static_cast<float>(y), static_cast<float>(px_per_cell), static_cast<float>(px_per_cell));
-                nvg::FillColor(nvg::RGBAf(0.6f, 0.6f, 0.6f, 1.0f));
-                nvg::Fill();
-                nvg::ClosePath();
-            }
+        nvg::BeginPath();
+        // draw grid lines
+        for (int i = 0; i <= grid_width; i+=px_per_cell) {
+            nvg::MoveTo(i, 0);
+            nvg::LineTo(i, grid_height);
+        }
+        for (int i = 0; i <= grid_height; i+=px_per_cell) {
+            nvg::MoveTo(0, i);
+            nvg::LineTo(grid_width, i);
+        }
+        nvg::StrokeColor(nvg::RGBAf(0.2f, 0.2f, 0.2f, 0.6f));
+        nvg::StrokeWidth(0.8f);
+        nvg::Stroke();
 
-                        nvg::BeginPath();
-            // draw grid lines
-            for (int i = 0; i <= grid_width; i+=px_per_cell) {
-                nvg::MoveTo(i, 0);
-                nvg::LineTo(i, grid_height);
-            }
-            for (int i = 0; i <= grid_height; i+=px_per_cell) {
-                nvg::MoveTo(0, i);
-                nvg::LineTo(grid_width, i);
-            }
-            nvg::StrokeColor(nvg::RGBAf(0.2f, 0.2f, 0.2f, 0.6f));
-            nvg::StrokeWidth(0.5f);
-            nvg::Stroke();
-            // nvg::FillColor(nvg::RGBA(255, 0, 0, 255));
-            // nvg::Fill();
-            nvg::ClosePath();
-    // nvgBeginPath(vg);
-    // nvgCircle(vg, fb_width/2+50, fb_height/2+50, 50);
-    // nvgFillColor(vg, nvgRGBA(255, 0, 125, 255));
-    // nvgFill(vg);
-
-    nvgEndFrame(vg);
-    
-    nvgluBindFramebuffer(NULL);
+    nvg::EndFrame();
+    nvg::GLUtilsBindFramebuffer(nullptr); 
         
-    // NVGpaint img_paint = nvgImagePattern(vg, 0, 0, 200, 200, 0, fb->image, 1.0f);
     NVGpaint img_paint = nvg::ImagePattern(0, 0, fb_width, fb_height, 0, fb->image, 1.0f);
     if (img_paint.image == 0) {
         std::cerr << "Failed to create image pattern." << std::endl;
         return -1;
     }
     
-    RenderModule::RegisterNanoVGCallback("Red Circle", [&](NVGcontext* vg) {
-        nvgBeginPath(vg);
-        nvgRect(vg, 0, 0, 200, 200);
-        nvgFillPaint(vg, img_paint);
-        nvgFill(vg);
-        nvgClosePath(vg);
+    // RenderModule::RegisterNanoVGCallback("Red Circle", [&](NVGcontext* vg) {
+    //     nvgBeginPath(vg);
+    //     nvgRect(vg, 0, 0, 200, 200);
+    //     nvgFillPaint(vg, img_paint);
+    //     nvgFill(vg);
+    //     nvgClosePath(vg);
 
-        // nvgluBindFramebuffer(fb);
-        // nvgBeginFrame(vg, fb->image, 200, 200, 1.0f);
-        nvgBeginPath(vg);
-        nvgCircle(vg, 100, 100, 50);
-        nvgFillColor(vg, nvgRGBA(255, 0, 0, 155));
-        nvgFill(vg);
-        // nvgEndFrame(vg);
-        // nvgluBindFramebuffer(NULL);
-    });
+    //     nvg::GLUtilsBindFramebuffer(fb);
+    //     glad::glViewport(0, 0, fb_width, fb_height);
+    //     // glad::glClearColor(0, 0, 0, 0);
+    //     // glad::glClear(GL_COLOR_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+    //     // nvgSave(vg);
+    //     // nvgluBindFramebuffer(fb);
+    //     nvgBeginFrame(vg, fb_width, fb_height, 1.0f);
+    //     nvgBeginPath(vg);
+    //     static float cx = 10;
+    //     static float cy = 10;
+    //     static float dx = 0.5f;
+    //     nvgCircle(vg, cx, cy, 5);
+    //     cx += dx;
+    //     cy += dx;
+    //     nvgFillColor(vg, nvgRGBA(255, 0, 0, 155));
+    //     nvgFill(vg);
+    //     nvgClosePath(vg);
+    //     nvgEndFrame(vg);
+    //     nvgluBindFramebuffer(NULL);
+    //     // nvgRestore(vg);
+    // });
 
     // NVGLUframebuffer *fb = nvgluCreateFramebuffer(vg, grid_width, grid_height, NVG_IMAGE_REPEATX | NVG_IMAGE_REPEATY);
     // if (!fb) {
@@ -173,71 +189,79 @@ int main() {
     // nvgluBindFramebuffer(nullptr);
 
 
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    // std::uniform_real_distribution<float> dist(0.0f, std::static_cast<float>(fb_width));
+    std::uniform_real_distribution<float> dist(0.0f, static_cast<float>(fb_width));
 
+    float test = 0.5f;
+    bool toggle = true;
+    int count = 1;
 
-    RenderModule::RegisterNanoVGCallback("Grid Map Viewer", [&](NVGcontext* vg) {
-        RenderModule::ZoomView([&](NVGcontext* vg) {
-            nvg::SetContext(vg);
+    RenderModule::RegisterImGuiCallback([&]() {
+        ImGui::Begin("Controls");
+        ImGui::Text("Grid Map Metadata:");
+        ImGui::SliderFloat("Test", &test, 0.0f, 1.0f);
+        ImGui::SliderInt("count", &count, 1, 100);
+        //create on off button
+        if (ImGui::Button(toggle ? "ON" : "OFF")) {
+            toggle = !toggle;
+        }
+        ImGui::End();
 
-            // nvg::BeginPath();
-            // nvg::Rect(0, 0, static_cast<float>(grid_width), static_cast<float>(grid_height));
-            // nvg::FillColor(nvg::RGBAf(1.0f, 1.0f, 1.0f, 1.0f));
-            // nvg::Fill();
-            // nvg::ClosePath();
-
-            // for (int i = 0; i < width * height; ++i) {
-            //     int x = (i % width) * px_per_cell;
-            //     int y = (i / width) * px_per_cell;
-            //     y = grid_height - y - px_per_cell; // Invert y-axis for correct rendering
-            //     unsigned char value = data.get()[i];
-            //     if (value != 0) {
-            //         continue;
-            //     }
-            //     nvg::BeginPath();
-            //     nvg::Rect(static_cast<float>(x), static_cast<float>(y), static_cast<float>(px_per_cell), static_cast<float>(px_per_cell));
-            //     nvg::FillColor(nvg::RGBAf(0.6f, 0.6f, 0.6f, 1.0f));
-            //     nvg::Fill();
-            //     nvg::ClosePath();
-            // }
-
-            // nvg::BeginPath();
-            // nvg::Rect(0, 0, width, height);
-            // // nvg::Rect(0, 0, static_cast<float>(grid_width), static_cast<float>(grid_height));
-            // nvg::FillColor(nvg::RGBA(0, 0, 0, 255));
-            // nvg::Fill();
-            // nvg::ClosePath();
-
-            // nvg::BeginPath();
-            // nvg::Rect(width+10, 0, width, height);
-            // nvg::FillColor(nvg::RGBA(0, 0, 0, 255));
-            // nvg::Fill();
-            // nvg::ClosePath();
-
-            // nvg::BeginPath();
-            // // draw grid lines
-            // for (int i = 0; i <= grid_width; i+=px_per_cell) {
-            //     nvg::MoveTo(i, 0);
-            //     nvg::LineTo(i, grid_height);
-            // }
-            // for (int i = 0; i <= grid_height; i+=px_per_cell) {
-            //     nvg::MoveTo(0, i);
-            //     nvg::LineTo(grid_width, i);
-            // }
-            // nvg::StrokeColor(nvg::RGBAf(0.2f, 0.2f, 0.2f, 0.6f));
-            // nvg::StrokeWidth(0.5f);
-            // nvg::Stroke();
-            // // nvg::FillColor(nvg::RGBA(255, 0, 0, 255));
-            // // nvg::Fill();
-            // nvg::ClosePath();
-
-            nvgBeginPath(vg);
-            nvgRect(vg, 0, 0, fb_width, fb_height);
-            nvgFillPaint(vg, img_paint);
-            nvgFill(vg);
-            nvgClosePath(vg);
-
-        });
+        // ImGui::Begin("Main Text Window", nullptr);
+        // ImGui::Begin("Main Text Window");
+        // ImGui::TextWrapped(
+        //     "CJK text will only appear if the font was loaded with the appropriate CJK character ranges. "
+        //     "Call io.Fonts->AddFontFromFileTTF() manually to load extra character ranges. "
+        //     "Read docs/FONTS.md for details.");
+        // ImGui::Text("Hiragana: \xe3\x81\x8b\xe3\x81\x8d\xe3\x81\x8f\xe3\x81\x91\xe3\x81\x93 (kakikukeko)");
+        // ImGui::Text("Kanjis: \xe6\x97\xa5\xe6\x9c\xac\xe8\xaa\x9e (nihongo)");
+        // static char buf[32] = "\xe6\x97\xa5\xe6\x9c\xac\xe8\xaa\x9e";
+        // ImGui::InputText("UTF-8 input", buf, IM_ARRAYSIZE(buf));
+        // ImGui::End();
     });
+
+    RenderModule::RegisterImGuiCallback([&]() {
+        ImGui::Begin("Grid Map Viewer");
+        ImGui::Text("Grid Map Metadata:");
+        ImGui::Text("Width: %d, Height: %d, Resolution: %.2f m/pixel", map_metadata.width, map_metadata.height, map_metadata.resolution);
+        ImGui::Text("Origin: (%.2f, %.2f)", map_metadata.origin_x, map_metadata.origin_y);
+        ImGui::Text(" ");
+        ImGui::End();
+    });
+
+    RenderModule::RegisterNanoVGCallback("Grid Map Viewer", 
+        /* Render */
+        [&](NVGcontext* vg) {
+            RenderModule::ZoomView([&](NVGcontext* vg) {
+                nvg::BeginPath();
+                nvg::Rect(0, 0, fb_width, fb_height);
+                nvg::FillPaint(img_paint);
+                nvg::Fill();
+            });
+        },
+        /* Offscreen Render */
+        [&](NVGcontext* vg) {
+            RenderModule::IsolatedFrameBuffer([&](NVGcontext* vg) {
+                if (toggle) {
+                    nvg::GLUtilsBindFramebuffer(fb);
+                    glad::glViewport(0, 0, fb_width, fb_height);
+
+                    nvg::BeginFrame(fb_width, fb_height, 1.0f);
+                    nvg::BeginPath();
+                    for (int i = 0; i < count; ++i) {
+                        nvg::Rect(std::floor(dist(gen)), std::floor(dist(gen)), px_per_cell, px_per_cell);
+                    }
+                    nvg::FillColor(nvg::RGBAf(1.0f, 0.0f, 0.0f, 1.0f));
+                    nvg::Fill();
+                    nvg::EndFrame();
+                    nvg::GLUtilsBindFramebuffer(nullptr);
+                }
+            });
+        }
+    );
+
     RenderModule::Run();
     RenderModule::Shutdown(); 
     // nvgluDeleteFramebuffer(fb);
